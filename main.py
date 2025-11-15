4a461bb7c24850504d1fb4b5ec521ee786402a2b
adnankar64-boop <adnankar64@gmail.com>
2025-11-15 18:30:15 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
 import os
 import sys
-import os
-import json
-import logging
-import re
-import threading
-import time
-from datetime import datetime, timezone
 
-
-# اضافه کردن مسیر پوشه فعلی
+# اضافه کردن مسیر پوشه فعلی (برای اینکه فایل اصلی پیدا شود)
 sys.path.append(os.path.dirname(__file__))
 
 from hyperdash_telegram_bot_mtproto_coinglass import main
